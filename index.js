34a634f4ca1992b6d0ea804dd10eb1d04355cbd8
Tomas Aparicio <tomas@aparicio.me>
2015-06-30 23:41:07 +0100

    refactor(route)

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function rocky(opts) {
   return new Rocky(opts)
 }
 
+rocky.httpProxy   = require('http-proxy')
 rocky.create      = require('./lib/create')
 rocky.middlewares = require('./lib/middlewares')
-rocky.httpProxy   = require('http-proxy')
 rocky.VERSION     = require('./package.json').version
