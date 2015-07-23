d4d19bc8cf7f385480abdad9b9bda3e6ec950139
Tomas Aparicio <tomas@aparicio.me>
2015-07-23 01:27:48 +0100

    feat(#58, #61)

---
+++ examples/retry.js
@@ -0,0 +1,53 @@
+var http = require('http')
+var rocky = require('..')
+
+var proxy = rocky()
+
+var customRetrytrategies = [
+  function (err, res) {
+    return !err && [404, 406].res.statusCode !== -1
+  }
+]
+
+// Enable retry logic (will be applicated for both forward/replay failed requests)
+proxy
+  .retry({
+    retries: 3,
+    factor: 2,
+    minTimeout: 100,
+    maxTimeout: 30 * 1000,
+    randomize: true,
+    strategies: customRetrytrategies
+  })
+
+proxy
+  .forward('http://127.0.0.1:9999') // invalid, will retry
+  .replay('http://127.0.0.1:9999') // invalid, will retry
+  .replay('http://127.0.0.1:3001') // valid
+
+// Subscribe to events
+proxy
+  .on('proxy:retry', function (err, res) {
+    console.log('Retry forward request:', err.code)
+  })
+  .on('replay:retry', function (err, res) {
+    console.log('Retry replay request:', err.code)
+  })
+
+proxy
+  .get('/*')
+
+proxy.listen(3000)
+
+http.createServer(function (req, res) {
+  setTimeout(function () {
+    console.log('Replay server is reached')
+    res.writeHead(204)
+    res.end()
+  }, 1000)
+}).listen(3001)
+
+// Test requests
+http.get('http://localhost:3000', function (res) {
+  console.log('Response from target server:', res.statusCode)
+})
