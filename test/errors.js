f87db27113c7e1be9bb8eef5cc1e3308b8712da6
Tomas Aparicio <tomas@aparicio.me>
2015-07-24 11:11:21 +0100

    feat: add error test

---
+++ test/errors.js
@@ -0,0 +1,19 @@
+const sinon = require('sinon')
+const expect = require('chai').expect
+const errors = require('../lib/errors')
+
+suite('errors', function () {
+  test('replyWithError', function (done) {
+    const spy = sinon.spy()
+    const err = { status: 500, message: 'omg' }
+    const res = { headersSent: false, end: spy, writeHead: spy }
+
+    errors.replyWithError(err, res)
+
+    expect(spy.calledTwice).to.be.true
+    expect(spy.args[0][0]).to.be.equal(err.status)
+    expect(spy.args[0][1]).to.be.deep.equal({ 'Content-Type': 'application/json' })
+    expect(spy.args[1][0]).to.be.equal('{"message":"omg"}')
+    done()
+  })
+})
