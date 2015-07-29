ab5bb9f40f23ebeedc75946d22a401bce5a87b42
Tomas Aparicio <tomas@aparicio.me>
2015-07-29 08:59:24 +0100

    feat(test): add retry suite

--- test/retry.js
+++ test/retry.js
@@ -1,25 +1,47 @@
 const sinon = require('sinon')
 const expect = require('chai').expect
-const passthrough = require('../lib/passthrough')
+const retry = require('../lib/retry')
 
 suite('retry', function () {
   test('error', function (done) {
     const spy = sinon.spy()
     const res = { }
-    const opts = { retry: 3 }
+    const opts = { retries: 3, factor: 2, minTimeout: 10 }
     const onRetry = spy
 
     var attempt = 0
     const task = function (done) {
-      if (attempt === 3) done()
+      if (attempt === 3) return done()
       attempt += 1
-      setTimeout(done, Math.random() * 5)
+      setTimeout(function () { done({ code: 'ECONNRESET' }) }, Math.random() * 5)
     }
 
-    const next = function () {
-
+    const next = function (err) {
+      expect(spy.calledThrice).to.be.true
+      done(err)
     }
 
-    done()
+    retry(opts, res, task, next, onRetry)
+  })
+
+  test('status', function (done) {
+    const spy = sinon.spy()
+    const res = { }
+    const opts = { retries: 3, factor: 2, minTimeout: 10 }
+    const onRetry = spy
+
+    var attempt = 0
+    const task = function (done) {
+      if (attempt === 3) return done()
+      attempt += 1
+      setTimeout(function () { done(null, { statusCode: 503 }) }, Math.random() * 5)
+    }
+
+    const next = function (err) {
+      expect(spy.calledThrice).to.be.true
+      done(err)
+    }
+
+    retry(opts, res, task, next, onRetry)
   })
 })
