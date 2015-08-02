c4f0dcf04a735a7b3a696f1c8374502f5a84f2ae
Tomas Aparicio <tomas@aparicio.me>
2015-08-02 11:41:25 +0100

    refactor(index): add comments

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-var Rocky = require('./lib/rocky')
+const Rocky = require('./lib/rocky')
 
 module.exports = rocky
+
+/**
+ * API factory
+ */
 
 function rocky(opts) {
   return new Rocky(opts)
 }
+
+/**
+ * Expose internal modules as static members
+ */
 
 rocky.Rocky          = Rocky
 rocky.Base           = require('./lib/base')
@@ -13,5 +21,15 @@ rocky.Dispatcher     = require('./lib/dispatcher')
 rocky.middleware     = require('./lib/middleware')
 rocky.passthrough    = require('./lib/passthrough')
 rocky.MiddlewarePool = require('./lib/mwpool')
-rocky.httpProxy      = require('http-proxy')
-rocky.VERSION        = require('./package.json').version
+
+/**
+ * Expose http-proxy module (mostly for hacking purposes)
+ */
+
+rocky.httpProxy = require('http-proxy')
+
+/**
+ * Expose current version
+ */
+
+rocky.VERSION = require('./package.json').version
