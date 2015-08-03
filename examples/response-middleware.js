94eaf6c53637528353b6805c28de40128815c63a
Tomas Aparicio <tomas@aparicio.me>
2015-08-03 10:54:34 +0100

    fix(middleware): response interceptor scopes. refactor. add example

---
+++ examples/response-middleware.js
@@ -0,0 +1,69 @@
+var http = require('http')
+var rocky = require('..')
+var supertest = require('supertest')
+
+// Creates the proxy
+var proxy = rocky()
+
+proxy
+  .forward('http://localhost:3001')
+  // Global response middleware
+  .useResponse(function (req, res, next) {
+    res.setHeader('Server', 'foo')
+    console.log('Modified body 2:', res.body.toString())
+    next()
+  })
+
+// Configure the route
+var route = proxy
+  .post('/foo')
+  // Replay traffic for the given route
+  .replay('http://localhost:3002', { replayOriginalBody: true })
+  // Subscribe to the server response to modify the body
+  .useResponse(function transformer(req, res, next) {
+    // Get the body buffer and parse it (assuming it's a JSON)
+    var body = JSON.parse(res.body.toString())
+
+    // Compose the new body
+    res.body = JSON.stringify({ greetings: 'hello ' + body.hello })
+
+    next()
+  })
+  .useResponse(function (req, res, next) {
+    res.setHeader('Powered-By', 'rocky')
+    console.log('Modified body:', res.body.toString())
+    next()
+  })
+
+proxy.listen(3000)
+
+// Target server
+http.createServer(function (req, res) {
+  console.log('Target server reached!')
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+
+  var body = ''
+  req.on('data', function (chunk) {
+    body += chunk
+  })
+  req.on('end', function () {
+    res.write(body)
+    res.end()
+  })
+}).listen(3001)
+
+// Replay server
+http.createServer(function (req, res) {
+  console.log('Replay server reached!')
+  res.writeHead(204)
+  res.end()
+}).listen(3002)
+
+// Client test request
+supertest('http://localhost:3000')
+  .post('/foo')
+  .send({'hello': 'world'})
+  .expect(200)
+  .end(function (err, res) {
+    console.log('Transformed body:', res.body)
+  })
