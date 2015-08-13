9b2f188d720613b97b552c1acabe1d9e62d558ca
Tomas Aparicio <tomas@aparicio.me>
2015-08-13 17:59:07 +0100

    feat(example): add https example

---
+++ examples/https.js
@@ -0,0 +1,23 @@
+var http = require('http')
+var rocky = require('..')
+
+var opts = {
+  ssl: {
+    key: fs.readFileSync('valid-ssl-key.pem', 'utf8'),
+    cert: fs.readFileSync('valid-ssl-cert.pem', 'utf8')
+  }
+}
+
+var proxy = rocky(opts)
+
+proxy
+  .forward('http://localhost:3001')
+  .all('/*')
+
+proxy.listen(3000)
+
+// Target server
+http.createServer(function (req, res) {
+  res.writeHead(200)
+  res.end()
+}).listen(3001)
