96820e586170b6177900bafcd97fddeb2f28bdfa
Tomas Aparicio <tomas@aparicio.me>
2015-08-20 19:06:18 +0100

    refactor(example): connect

--- examples/connect.js
+++ examples/connect.js
@@ -3,12 +3,14 @@ var connect = require('connect')
 var rocky = require('..')
 
 var app = connect()
-var migrate = rocky()
+var proxy = rocky()
 
-migrate
+// Configure the proxy and routes
+proxy
   .forward('http://localhost:3001')
 
-migrate.get('/users/:id')
+proxy
+  .get('/*')
   .replay('http://localhost:3002')
   .replay('http://localhost:3002')
   .on('request', function (opts) {
@@ -18,12 +20,14 @@ migrate.get('/users/:id')
     console.log('Error:', err)
   })
 
-app.use(migrate.middleware())
+// Plug in rocky middleware in connect
+app.use(proxy.middleware())
 app.listen(3000)
 
 // Target server
 connect()
   .use(function (req, res) {
+    console.log('Target server reached')
     res.end('Hello World!')
   })
   .listen(3001)
@@ -31,15 +35,15 @@ connect()
 // Replay server
 connect()
   .use(function (req, res) {
-    console.log('Not found')
+    console.log('Replay server reached')
     res.statusCode = 404
     res.end()
   })
   .listen(3002)
 
 // Test request
-http.get('http://localhost:3000/users/pepe', function (res) {
-  console.log('Status:', res.statusCode)
+http.get('http://localhost:3000/', function (res) {
+  console.log('Response status:', res.statusCode)
 
   res.on('data', function (chunk) {
     console.log('Body: ' + chunk)
