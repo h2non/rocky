f5c987b986aa11b32a23bc9c3617360aba05b7ee
Tomas Aparicio <tomas@aparicio.me>
2015-08-23 13:47:36 +0100

    feat(examples): add replay example

---
+++ examples/replay.js
@@ -0,0 +1,39 @@
+var http = require('http')
+var rocky = require('..')
+
+var proxy = rocky()
+
+// Forward to an invalid server
+proxy
+  .forward('http://invalid:9321')
+
+// Configure the replay servers
+proxy
+  .replay('http://localhost:3002')
+  .replay('http://localhost:3003')
+
+proxy
+  .get('/*')
+
+proxy.listen(3000)
+
+http.createServer(function (req, res) {
+  setTimeout(function () {
+    console.log('Replay server 1 reached!')
+    res.writeHead(204)
+    res.end()
+  }, 1000)
+}).listen(3002)
+
+http.createServer(function (req, res) {
+  setTimeout(function () {
+    console.log('Replay server 2 reached!')
+    res.writeHead(204)
+    res.end()
+  }, 1000)
+}).listen(3003)
+
+// Test requests
+http.get('http://localhost:3000/', function (res) {
+  console.log('Response from target server:', res.statusCode)
+})
