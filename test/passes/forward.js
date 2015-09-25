a456bff254fa1d9dc35df216c4df45fc846f7ec1
Tomas Aparicio <tomas@aparicio.me>
2015-09-25 12:14:01 +0100

    fix(forward): use the proper host header

--- test/passes/forward.js
+++ test/passes/forward.js
@@ -34,11 +34,49 @@ suite('forward', function () {
       server.close(done)
     }
   })
+
+  test('forward host', function (done) {
+    var spy = sinon.spy()
+    var host = 'localhost:' + port
+    var server = createServer(port, assertServer)
+    var route = new Route('/')
+    var res = new ResponseStub()
+    var opts = { forwardHost: true, target: 'http://127.0.0.1:' + port }
+
+    var req = new http.IncomingMessage(new Socket)
+    req.rocky = { options: {} }
+    req.push('foo')
+    req.push(null)
+
+    route.useForward(assertMiddleware)
+    route.on('proxyRes', spy)
+    route.on('proxyReq', spy)
+
+    forward(route, opts, req, res, assert)
+
+    function assertMiddleware(req, res, next) {
+      req.headers.host = host
+      spy(); next()
+    }
+
+    function assertServer(req, res) {
+      spy()
+      expect(req.headers.host).to.be.equal(host)
+    }
+
+    function assert(err, res) {
+      expect(spy.args).to.have.length(4)
+      expect(err).to.be.null
+      expect(res).to.be.an('object')
+      server.close(done)
+    }
+  })
 })
 
-function createServer(port) {
+function createServer(port, assert) {
   var server = http.createServer(function (req, res) {
     res.setHeader('test', 'forward')
+    if (assert) assert(req, res)
     res.end()
   })
   server.listen(port)
---
+++ test/streaming.js
@@ -0,0 +1,10 @@
+const http = require('http')
+const sinon = require('sinon')
+const supertest = require('supertest')
+const request = require('request')
+const expect = require('chai').expect
+const rocky = require('..')
+
+suite('streaming', function () {
+  // to do
+})
